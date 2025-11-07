5750c93a862bbf83c374c7e64e47ff86ee989d6e
tombunzel <bunzel.tom@gmail.com>
2025-11-07 23:09:02 +0100

    fix(main): add missing time import

--- json.go
+++ json.go
@@ -30,8 +30,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	_, err := w.Write(dat)
-	if err != nil {
-		log.Printf("Error writing response: %s", err)
+	_, wErr := w.Write(dat)
+	if wErr != nil {
+		log.Printf("Error writing response: %s", wErr)
 	}
 }
